62c42a9719b469b8aa34979ea5a616a641dd0cc9
sukonlawatporn-source <sukonlawat.porn@bumail.net>
2026-05-18 23:35:48 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import sqlite3
 
 app = Flask(__name__)
 
+# ================= HOME =================
+
 @app.route('/')
 def home():
 
@@ -19,6 +21,8 @@ def home():
         'index.html',
         customers=customers
     )
+
+# ================= ADD CUSTOMER =================
 
 @app.route('/add', methods=['POST'])
 def add_customer():
@@ -40,6 +44,8 @@ def add_customer():
 
     return redirect('/')
 
+# ================= DELETE CUSTOMER =================
+
 @app.route('/delete/<int:id>')
 def delete_customer(id):
 
@@ -55,6 +61,8 @@ def delete_customer(id):
     conn.close()
 
     return redirect('/')
+
+# ================= EDIT CUSTOMER =================
 
 @app.route('/edit/<int:id>')
 def edit_customer(id):
@@ -75,6 +83,8 @@ def edit_customer(id):
         'edit.html',
         customer=customer
     )
+
+# ================= UPDATE CUSTOMER =================
 
 @app.route('/update/<int:id>', methods=['POST'])
 def update_customer(id):
@@ -99,6 +109,91 @@ def update_customer(id):
     conn.close()
 
     return redirect('/')
+
+# ================= GAMES PAGE =================
+
+@app.route('/games')
+def games():
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM games")
+
+    games = cursor.fetchall()
+
+    conn.close()
+
+    return render_template(
+        'games.html',
+        games=games
+    )
+
+# ================= DELETE GAME =================
+
+@app.route('/delete_game/<int:id>')
+def delete_game(id):
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "DELETE FROM games WHERE game_id = ?",
+        (id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return redirect('/games')
+
+# ================= EDIT GAME =================
+
+@app.route('/edit_game/<int:id>')
+def edit_game(id):
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM games WHERE game_id = ?",
+        (id,)
+    )
+
+    game = cursor.fetchone()
+
+    conn.close()
+
+    return render_template(
+        'edit_game.html',
+        game=game
+    )
+
+# ================= UPDATE GAME =================
+
+@app.route('/update_game/<int:id>', methods=['POST'])
+def update_game(id):
+
+    game_name = request.form['game_name']
+    type = request.form['type']
+    size_gb = request.form['size_gb']
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        UPDATE games
+        SET game_name = ?, type = ?, size_gb = ?
+        WHERE game_id = ?
+        """,
+        (game_name, type, size_gb, id)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return redirect('/games')
 
 if __name__ == '__main__':
     app.run(debug=True)
